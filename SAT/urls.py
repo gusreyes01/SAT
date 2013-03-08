362fd9f169db6c59565342d475425f9aaa56160c
Gustavo Reyes <gusreyes01@gmail.com>
2013-03-08 13:38:43 -0600

    Updatex

--- SAT/urls.py
+++ SAT/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
      
      url(r'^accounts/login/$',  login),
-     url(r'^accounts/login/$',  logout),
+     url(r'^accounts/logout/$',  logout),
 )
 
 # Add the static files pattern to the url.
--- app/views.py
+++ app/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return render_to_response('home/home.html', context_instance=RequestContext(request))
 
