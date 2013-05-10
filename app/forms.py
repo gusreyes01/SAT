3ec2ddeb20183184bf7b48781d5362e4fd0dbf7c
Rafael <rafael@mail.com>
2013-05-10 12:06:32 -0500

    Encuetas_FrontEnd_Revision

--- app/forms.py
+++ app/forms.py
@@ -38,25 +38,85 @@ ESTADO_ANTIDOPING = (
     ('5','Antidoping terminado'),
 )
 
-SEMESTRE = (
-    ('1-3', '1° - 3°'),
-    ('4-6', '4° - 6°'),
-    ('7-8', '7° - 8°'),
-    ('9', '9°'),
+SI_NO = (
+    ('0','Sí'),
+    ('1','No'),
 )
 
-CONSUMIDO = (
-    ('Escuela', 'Escuela'),
-    ('Trabajo', 'Trabajo'),
-    ('Fiesta', 'Fiesta'),
-    ('Otros', 'Otros'),
+ALCOHOL_FRECUENCIA = (
+    ('0','No consumo'),
+    ('1','Una vez a la semana o menos'),
+    ('2','Dos veces a la semana'),
+    ('3','Más de dos veces por semana'),
 )
 
-FRECUENCIA = (
-    ('1 dia', '1 dia'),
-    ('4 dias', '4 dias'),
-    ('1 semana', '1 semana'),
-    ('1 mes', '1 mes'),
+TABACO_FRECUENCIA = (
+    ('0','No consumo'),
+    ('1','Menos de 5 cigarros al día'),
+    ('2','De 5 a 10 cigarros al día'),
+    ('3','De 10 cigarrillos a una cajetilla al día'),
+    ('4','Más de una cajetilla al día'),
+)
+
+SI_NO_2 = (
+    ('0','Sí'),
+    ('1','No (Pasa a la pregunta 8)'),
+)
+
+QUIEN_OFRECIDO = (
+    ('0','Un compañero de la escuela'),
+    ('1','Un amigo'),
+    ('2','Un vecino'),
+    ('3','Un conocido'),
+    ('4','Un familiar'),
+    ('5','Otro'),
+)
+
+DONDE_OFRECIDO = (
+    ('0','En un antro, bar, concierto o fiesta'),
+    ('1','En un viaje al extranjero'),
+    ('2','En casa de algún amigo o compañero'),
+    ('3','En la escuela'),
+    ('4','En la calle'),
+    ('5','Otro'),
+)
+
+QUE_OFRECIDO_CONSUMIDO = (
+    ('0','Marihuana'),
+    ('1','Cocaína'),
+    ('2','Anfetaminas, Metanfetaminas'),
+    ('3','Alucinógenos'),
+    ('4','Opiáceos'),
+    ('5','Otro'),
+)
+
+SI_NO_3 = (
+    ('0','Sí'),
+    ('1','No (Pasa a la pregunta 13)'),
+)
+
+ULTIMO_CONSUMIDO = (
+    ('0','En los últimos 30 días'),
+    ('1','Hace más de un mes y menos de cinco meses'),
+    ('2','Hace más de seis meses'),
+)
+
+QUIEN_OFRECIDO = (
+    ('0','Un compañero de la escuela'),
+    ('1','Un amigo'),
+    ('2','Un vecino'),
+    ('3','Un conocido'),
+    ('4','Un familiar'),
+    ('5','Otro'),
+)
+
+LUGAR_CONSUMIDO = (
+    ('0','En un antro, bar, concierto o fiesta'),
+    ('1','En un viaje al extranjero'),
+    ('2','En casa de algún amigo o compañero'),
+    ('3','En la escuela'),
+    ('4','En la calle'),
+    ('5','Otro'),
 )
 
 COLOR = (
@@ -232,11 +292,30 @@ class EvaluaEstudiante(ModelForm):
 
 class AplicacionEncuesta(ModelForm):
   folio = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}), error_messages=my_default_errors, label="Folio", required=False)
-  correo = forms.EmailField(error_messages=my_default_errors,label="Correo",required=True)
-  semestre = forms.ChoiceField(widget=forms.RadioSelect(), choices=SEMESTRE, required = True, label="Semestre")
-  opinion = forms.CharField(widget=forms.Textarea(), required = False,label="Opinion")
-  frecuencia = forms.ChoiceField(error_messages=my_default_errors, choices=FRECUENCIA, required = True, label="Frecuencia")
-  
+  medicamento_consumido = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="1. ¿Actualmente consumes algún medicamento?",required=False)
+  medicamento_consumido_paraque = forms.CharField(widget=forms.Textarea(attrs={'rows':4, 'cols':40}), label="Si respondiste 'Sí', ¿cuál y para qué?", required = False)
+  alcohol_frecuencia = forms.ChoiceField(widget=forms.RadioSelect(), choices=ALCOHOL_FRECUENCIA, label="2. ¿Con qué frecuencia consumes bebidas alcohólicas?",required=False)
+  tabaco_frecuencia = forms.ChoiceField(widget=forms.RadioSelect(), choices=TABACO_FRECUENCIA, label="3. ¿Con qué frecuencia consumes cigarros de tabaco?",required=False)
+  droga_ofrecido = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO_2, label="4. ¿Ten han ofrecido alguna vez alguna droga ilegal?",required=False)
+  quien_ofrecido = forms.ChoiceField(widget=forms.RadioSelect(), choices=QUIEN_OFRECIDO, label="5. ¿Quién te ha ofrecido?",required=False)
+  quien_ofrecido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica quién", required=False)
+  donde_ofrecido = forms.ChoiceField(widget=forms.RadioSelect(), choices=DONDE_OFRECIDO, label="6. ¿En dónde te han ofrecido?",required=False)
+  donde_ofrecido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica dónde", required=False)  
+  que_ofrecido = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), choices=QUE_OFRECIDO_CONSUMIDO, label="7. ¿Qué te ofrecieron? (Puedes seleccionar más de una opción)", required = False)
+  que_ofrecido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica qué", required=False)
+  haz_consumido = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO_3, label="8. ¿Has consumido alguna vez alguna droga ilegal?",required=False)
+  edad_consumido = forms.CharField(widget=forms.TextInput(), label="9. ¿A qué edad consumiste por primera vez?", required=False)
+  que_consumido = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), choices=QUE_OFRECIDO_CONSUMIDO, label="10. ¿Qué consumiste? (Puedes seleccionar más de una opción)", required = False)
+  que_consumido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica qué", required=False)
+  ultimo_consumido = forms.ChoiceField(widget=forms.RadioSelect(), choices=ULTIMO_CONSUMIDO, label="11. ¿Cuándo fue tu último consumo? (Recuerda que adicionalmente a este cuestionario se te aplicará una prueba de dopaje)", required=False)
+  que_consumido2 = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), choices=QUE_OFRECIDO_CONSUMIDO, label="12. ¿Qué consumiste? (Puedes seleccionar más de una opción)", required = False)
+  que_consumido2_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica qué", required=False)
+  conoces_consumidor = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="13. ¿Conoces a alguien que consuma drogas ilegales?", required=False)
+  de_donde = forms.ChoiceField(widget=forms.RadioSelect(), choices=QUIEN_OFRECIDO, label="14. Si respondiste afirmativamente a la pregunta anterior, indica de donde:", required=False)  
+  de_donde_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica quién", required=False)
+  lugar_consumo = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="15. ¿Has estado en algún lugar donde a tu alrededor se consuman drogas ilegales?", required=False)
+  lugar_consumo_donde = forms.ChoiceField(widget=forms.RadioSelect(), choices=LUGAR_CONSUMIDO, label="16. Si respondiste afirmativamente a la pregunta anterior, indica de donde:", required=False)  
+  relaciones = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="17. ¿Has tenido relaciones sexuales en los últimos 30 días?", required=False)
   class Meta:
     model = EstudianteMuestra
     exclude = ('resultado', 'antidoping', 'inscrito', 'tipo_seleccion', 'tipo_droga', 'estado','respuestas', 'notas')
@@ -248,30 +327,68 @@ class AplicacionEncuesta(ModelForm):
       self.helper.form_method = 'POST'
       self.helper.layout = Layout(
 	Div(
-	Div('folio',
-	    'correo',
-	    'semestre',
-	    css_class='span3'),
-	Div(#'consumido',
-	    'opinion',
-	    'frecuencia',
-	    css_class='span3'),css_class='row-fluid'),
+	Div('folio',),
+	Div('medicamento_consumido',
+	    'medicamento_consumido_paraque',
+	    'alcohol_frecuencia',
+	    'tabaco_frecuencia',
+	    'droga_ofrecido',
+	    'quien_ofrecido',
+	    'quien_ofrecido_otro',
+	    'donde_ofrecido',
+	    'donde_ofrecido_otro',
+	    'que_ofrecido',
+	    'que_ofrecido_otro',
+	    'haz_consumido',
+	    'edad_consumido',
+	    css_class='span5'),
+	Div('que_consumido',
+	    'que_consumido_otro',
+	    'ultimo_consumido',
+	    'que_consumido2',
+	    'que_consumido2_otro',
+	    'conoces_consumidor',
+	    'de_donde',
+	    'de_donde_otro',
+	    'lugar_consumo',
+	    'lugar_consumo_donde',
+	    'relaciones',
+	    css_class='span5'),css_class='row-fluid'),
 	ButtonHolder(
 	    Submit('submit', 'Enviar respuestas', css_class='btn-success')
 	))
       super(AplicacionEncuesta, self).__init__(*args, **kwargs)
 
 class EncuestaContestada(ModelForm):
-  folio = forms.CharField(error_messages=my_default_errors, label="Folio", required=False)
-  correo = forms.EmailField(error_messages=my_default_errors,label="Correo",required=True)
-  semestre = forms.CharField(error_messages=my_default_errors,label="Semestre",required=True)
-  opinion = forms.CharField(widget=forms.Textarea(), required = False,label="Opinion")
-  frecuencia = forms.CharField(error_messages=my_default_errors,label="Frecuencia",required=True)
-  notas = forms.CharField(widget=forms.Textarea(), required = False,label="Notas")
-  
+  folio = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}), error_messages=my_default_errors, label="Folio", required=False)
+  medicamento_consumido = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="1. ¿Actualmente consumes algún medicamento?",required=False)
+  medicamento_consumido_paraque = forms.CharField(widget=forms.Textarea(attrs={'rows':4, 'cols':40}), label="Si respondiste 'Sí', ¿cuál y para qué?", required = False)
+  alcohol_frecuencia = forms.ChoiceField(widget=forms.RadioSelect(), choices=ALCOHOL_FRECUENCIA, label="2. ¿Con qué frecuencia consumes bebidas alcohólicas?",required=False)
+  tabaco_frecuencia = forms.ChoiceField(widget=forms.RadioSelect(), choices=TABACO_FRECUENCIA, label="3. ¿Con qué frecuencia consumes cigarros de tabaco?",required=False)
+  droga_ofrecido = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO_2, label="4. ¿Ten han ofrecido alguna vez alguna droga ilegal?",required=False)
+  quien_ofrecido = forms.ChoiceField(widget=forms.RadioSelect(), choices=QUIEN_OFRECIDO, label="5. ¿Quién te ha ofrecido?",required=False)
+  quien_ofrecido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica quién", required=False)
+  donde_ofrecido = forms.ChoiceField(widget=forms.RadioSelect(), choices=DONDE_OFRECIDO, label="6. ¿En dónde te han ofrecido?",required=False)
+  donde_ofrecido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica dónde", required=False)  
+  que_ofrecido = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), choices=QUE_OFRECIDO_CONSUMIDO, label="7. ¿Qué te ofrecieron? (Puedes seleccionar más de una opción)", required = False)
+  que_ofrecido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica qué", required=False)
+  haz_consumido = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO_3, label="8. ¿Has consumido alguna vez alguna droga ilegal?",required=False)
+  edad_consumido = forms.CharField(widget=forms.TextInput(), label="9. ¿A qué edad consumiste por primera vez?", required=False)
+  que_consumido = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), choices=QUE_OFRECIDO_CONSUMIDO, label="10. ¿Qué consumiste? (Puedes seleccionar más de una opción)", required = False)
+  que_consumido_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica qué", required=False)
+  ultimo_consumido = forms.ChoiceField(widget=forms.RadioSelect(), choices=ULTIMO_CONSUMIDO, label="11. ¿Cuándo fue tu último consumo? (Recuerda que adicionalmente a este cuestionario se te aplicará una prueba de dopaje)", required=False)
+  que_consumido2 = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(), choices=QUE_OFRECIDO_CONSUMIDO, label="12. ¿Qué consumiste? (Puedes seleccionar más de una opción)", required = False)
+  que_consumido2_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica qué", required=False)
+  conoces_consumidor = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="13. ¿Conoces a alguien que consuma drogas ilegales?", required=False)
+  de_donde = forms.ChoiceField(widget=forms.RadioSelect(), choices=QUIEN_OFRECIDO, label="14. Si respondiste afirmativamente a la pregunta anterior, indica de donde:", required=False)  
+  de_donde_otro = forms.CharField(widget=forms.TextInput(), label="Si responsite 'Otro' especifica quién", required=False)
+  lugar_consumo = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="15. ¿Has estado en algún lugar donde a tu alrededor se consuman drogas ilegales?", required=False)
+  lugar_consumo_donde = forms.ChoiceField(widget=forms.RadioSelect(), choices=LUGAR_CONSUMIDO, label="16. Si respondiste afirmativamente a la pregunta anterior, indica de donde:", required=False)  
+  relaciones = forms.ChoiceField(widget=forms.RadioSelect(), choices=SI_NO, label="17. ¿Has tenido relaciones sexuales en los últimos 30 días?", required=False)
+  notas = forms.CharField(widget=forms.Textarea(), required = False,label="Notas")  
   class Meta:
     model = EstudianteMuestra
-    exclude = ('id', 'folio', 'respuestas')
+    exclude = ('resultado', 'antidoping', 'inscrito', 'tipo_seleccion', 'tipo_droga', 'estado','respuestas')
         
   def __init__(self, *args, **kwargs):
       self.helper = FormHelper()
@@ -280,15 +397,34 @@ class EncuestaContestada(ModelForm):
       self.helper.form_method = 'POST'
       self.helper.layout = Layout(
 	Div(
-	Div('folio',
-	    'correo',
-	    'semestre',
-	    css_class='span3'),
-	Div(#'consumido',
-	    'opinion',
-	    'frecuencia',
+	Div('folio',),
+	Div('medicamento_consumido',
+	    'medicamento_consumido_paraque',
+	    'alcohol_frecuencia',
+	    'tabaco_frecuencia',
+	    'droga_ofrecido',
+	    'quien_ofrecido',
+	    'quien_ofrecido_otro',
+	    'donde_ofrecido',
+	    'donde_ofrecido_otro',
+	    'que_ofrecido',
+	    'que_ofrecido_otro',
+	    'haz_consumido',
+	    'edad_consumido',
+	    css_class='span5'),
+	Div('que_consumido',
+	    'que_consumido_otro',
+	    'ultimo_consumido',
+	    'que_consumido2',
+	    'que_consumido2_otro',
+	    'conoces_consumidor',
+	    'de_donde',
+	    'de_donde_otro',
+	    'lugar_consumo',
+	    'lugar_consumo_donde',
+	    'relaciones',
 	    'notas',
-	    css_class='span3'),css_class='row-fluid'),
+	    css_class='span5'),css_class='row-fluid'),
 	ButtonHolder(
 	    Submit('submit', 'Subir notas', css_class='btn-success')
 	))
